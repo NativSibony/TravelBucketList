6108c7df8b3e6f054cfe28c5b4a08d6ae5406d4c
nativsibony <nativs@nucleon.sh>
2021-05-19 10:43:47 +0300

    .

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/locations", {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
